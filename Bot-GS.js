13dbac7d6736ddb7a91742042cc9b7a6194a9d61
GalkaSemen <116259158+GalkaSemen@users.noreply.github.com>
2022-11-17 12:01:00 +0300

    YandexBot
    Находит и заходит на сайт

--- Bot-GS.js
+++ Bot-GS.js
@@ -3,18 +3,20 @@
     // @namespace    http://tampermonkey.net/
     // @version      0.1
     // @description  try to take over the world!
-    // @author        Galina Semenova
+    // @author       Galina Semenova
     // @match        https://ya.ru/*
     // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
     // @grant        none
     // ==/UserScript==
 
-    let keywords = ["Покупка автомобиля ", "Купля-продажа авто", "Автомобиль с пробегом", "Каталог автомобилей"];
-    let keyword = keywords[getRandom(0, keywords.length)];
-    let btnK = document.getElementsByName("btnK")[0];
     let links = document.links;
+    let btnK = document.getElementsByName("btnK")[0];
+    let keywords = ["Покупка автомобиля ", "купля-продажа авто", "Автомобиль с пробегом", "каталог автомобилей"];
+    let keyword = keywords[getRandom(0, keywords.length)];
 
-    if(btnK !== undefined) {
+
+
+   if(btnK !== undefined) {
       document.getElementsByName("q")[0].value = keyword;
       document.getElementsByName("btnK")[0].click();
     } else {
