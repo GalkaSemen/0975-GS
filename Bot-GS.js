4709213be52fa4c680f38de2b0db493097828197
GalkaSemen <116259158+GalkaSemen@users.noreply.github.com>
2022-11-17 11:48:41 +0300

    YandexBot
    Находит и переходит на сайт

--- Bot-GS.js
+++ Bot-GS.js
@@ -1,33 +1,33 @@
 // ==UserScript==
-// @name         Google Bot
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  Bot for Google
-// @author       Galina Semenova
-// @match        https://www.google.com/*
-// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
-// @grant        none
-// ==/UserScript==
+    // @name         YandexBot
+    // @namespace    http://tampermonkey.net/
+    // @version      0.1
+    // @description  try to take over the world!
+    // @author        Galina Semenova
+    // @match        https://ya.ru/*
+    // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+    // @grant        none
+    // ==/UserScript==
 
-let links = document.links;
-let btnK = document.getElementsByName("btnK")[0];
-let keywords = ["10 самых популярных шрифтов от Google", "Отключение редакций и ревизий в WordPress", "Вывод произвольных типов записей и полей в WordPress"];
-let keyword = keywords[getRandom(0, keywords.length)]
+    let keywords = ["Покупка автомобиля ", "купля-продажа авто", "Автомобиль с пробегом", "каталог автомобилей"];
+    let keyword = keywords[getRandom(0, keywords.length)];
+    let btnK = document.getElementsByName("btnK")[0];
+    let links = document.links;
 
-if (btnK !== undefined) {
-  document.getElementsByName("q")[0].value = keyword;
-  btnK.click();
-} else {
-for (let i = 0; i < links.length; i++) {
-    if (links[i].href.indexOf("napli.ru") !== -1) {
-    let link=links[i];
-        console.log("Нашел строку " + links[i]);
-        link.click();
-        break;
+    if(btnK !== undefined) {
+      document.getElementsByName("q")[0].value = keyword;
+      document.getElementsByName("btnK")[0].click();
+    } else {
+      for (let i = 0; i < links.length; i++) {
+        if (links[i].href.indexOf("auto.ru") !== -1) {
+          let link = links[i]
+          console.log("Нашел строку " + links[i]);
+          link.click();
+          break;
+        }
+      }
     }
-  }
-}
 
-function getRandom (min, max) {
-return Math.floor(Math.random()*(max-min)+min);
-}
+    function getRandom(min, max) {
+      return Math.floor(Math.random() * (max - min) + min);
+    }
