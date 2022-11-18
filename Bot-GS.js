27229482e56fb89a6889aabfac2d8d3aac9c5123
GalkaSemen <116259158+GalkaSemen@users.noreply.github.com>
2022-11-18 12:19:39 +0300

    YandexBot
    Вводит в поиске, находит и переходит на сайт.

--- Bot-GS.js
+++ Bot-GS.js
@@ -5,30 +5,33 @@
     // @description  try to take over the world!
     // @author       Galina Semenova
     // @match        https://ya.ru/*
+    // @match        https://yandex.ru/*
+    // @match        https://yandex.ru/search/
     // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
     // @grant        none
     // ==/UserScript==
 
-    let links = document.links;
-    let btnK = document.getElementsByName("btnK")[0];
-    let keywords = ["Покупка автомобиля ", "купля-продажа авто", "Автомобиль с пробегом", "каталог автомобилей"];
-    let keyword = keywords[getRandom(0, keywords.length)];
 
+let links = document.links;
+let button = document.getElementsByClassName("search3__button mini-suggest__button")[0];
+let keywords = ["Покупка автомобиля ", "купля-продажа авто", "Автомобиль с пробегом", "каталог автомобилей"];
+let keyword = keywords[getRandom(0, keywords.length)];
 
+if (button !== undefined) {
+ document.getElementsByName("text")[0].value = keyword;
+ button.click();
 
-   if(btnK !== undefined) {
-      document.getElementsByName("q")[0].value = keyword;
-      document.getElementsByName("btnK")[0].click();
-    } else {
-      for (let i = 0; i < links.length; i++) {
-        if (links[i].href.indexOf("auto.ru") !== -1) {
-          let link = links[i]
-          console.log("Нашел строку " + links[i]);
-          link.click();
-          break;
-        }
-      }
+} else {
+  for( let i =0; i< links.length; i++){
+    if (links[i].href.indexOf("auto.ru") !== -1){
+      let link = links[i]
+      console.log("Нашел строку " + links[i])
+      link.click();
+      break;
     }
+  }
+}
+
 
     function getRandom(min, max) {
       return Math.floor(Math.random() * (max - min) + min);
